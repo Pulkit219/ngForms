16d5981b2f7f3ffb498b7eb3a75be9e89d0c4a2f
Pulkit219 <ascension1110@gmail.com>
2017-11-29 19:50:59 -0500

    5th comm
    ng modDrivenForm  form group and form control added

--- modDrivenForm/src/app/app.module.ts
+++ modDrivenForm/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { SignupFormComponent } from './../../signup-form/signup-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
